99ad816eb0c02fdbb3ec85df85a6def63ead49c6
fud0ch1 <livefromtheabyss@gmail.com>
2019-02-28 16:24:21 -0500

    Added project page

--- server.js
+++ server.js
@@ -57,11 +57,17 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
-  })
-})
+  });
+});
 // Heroku environment variable
 app.listen(port, () => {
   console.log(`Server is up on ${port}`)
